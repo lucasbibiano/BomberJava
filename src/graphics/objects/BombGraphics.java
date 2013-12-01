34d1992a80d3f5f49627959f017ddf40ab06591a
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-12-01 20:40:42 -0300

    Updates bomb graphics

--- src/graphics/objects/BombGraphics.java
+++ src/graphics/objects/BombGraphics.java
@@ -5,6 +5,11 @@ import static constants.Constants.TILESIZE;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import game_objects.Bomb;
 import game_objects.GameObject;
@@ -23,6 +28,17 @@ public class BombGraphics extends GameObjectGraphics {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setColor(bomb.isExploded() ? Color.YELLOW : Color.black);
 		
-		g2d.fillOval(bomb.getX() * TILESIZE + 4, bomb.getY() * TILESIZE + 4, 32 - 8, 32 - 8);
+		BufferedImage img = null;
+		String url = System.getProperty("user.dir");
+		url+="/src/sprites/bomb.gif";
+		
+		try {
+		    img = ImageIO.read(new File(url));
+		} catch (IOException e) {
+		}
+		
+		g2d.drawImage(img, bomb.getX() * TILESIZE, bomb.getY() * TILESIZE, null);
+		
+		//g2d.fillOval(bomb.getX() * TILESIZE + 4, bomb.getY() * TILESIZE + 4, 32 - 8, 32 - 8);
 	}
 }
