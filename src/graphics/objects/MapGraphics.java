5588650eabcecdb03a7fac03ffb6e2f21fbcc6e4
Roger <luizrogeriocn@gmail.com>
2013-12-01 23:11:31 -0300

    adding grass sprites

--- src/graphics/objects/MapGraphics.java
+++ src/graphics/objects/MapGraphics.java
@@ -4,6 +4,11 @@ import game_objects.Map;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import static constants.Constants.*;
 
@@ -18,9 +23,19 @@ public class MapGraphics implements Drawable {
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(Color.BLACK);
+		BufferedImage img = null;
+		String url = System.getProperty("user.dir");
+		url+="/src/sprites/grass.png";
+		
+		try {
+		    img = ImageIO.read(new File(url));
+		} catch (IOException e) {
+		}
+		
 		for (int i = 0; i < map.getHeight(); i++) {
 			for (int j = 0; j < map.getWidth(); j++) {
-				g.drawRect(j * TILESIZE, i * TILESIZE, TILESIZE, TILESIZE);
+				//g.drawRect(j * TILESIZE, i * TILESIZE, TILESIZE, TILESIZE);
+				g.drawImage(img, j * TILESIZE, i * TILESIZE, null);
 			}
 		}
 	}
